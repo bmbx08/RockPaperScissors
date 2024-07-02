9dd46f1cb6d380906d84850be5f92c407fa46fdd
bmbx08 <bmbx08@bu.ac.kr>
2024-07-03 04:43:12 +0900

    finished game

--- src/component/Box.js
+++ src/component/Box.js
@@ -1,18 +1,25 @@
-import React from 'react'
+import React from "react";
 
 const Box = (props) => {
+  let result;
+  if (
+    props.title === "Computer" &&
+    props.result !== "Tie" &&
+    props.result !== ""
+  ){
+    result = props.result === "Win" ? "Lose" : "Win";
+  }else {
+    result = props.result;
+  }
+
   return (
     <div className="box">
-        <h1>{props.title}</h1>
-        <img className="item-img" src="https://upload.wikimedia.org/wikipedia/commons/thumb/5/5f/Rock-paper-scissors_%28scissors%29.png/1200px-Rock-paper-scissors_%28scissors%29.png"/>
-        <h2>Win</h2>
+      <h1>{props.title}</h1>
+      <img className="item-img" src={props.item && props.item.img} />
+      {/*'props.item&&'는 에러 방지용으로 넣는다.*/}
+      <h2 className={`${result}`}>{result}</h2>
     </div>
-    // <div className="box">
-    //   <h2>{props.user}</h2>
-    //   <img src={props.src}/>
-    //   {/* <h2>{result}</h2> */}
-    // </div>
-  )
-}
+  );
+};
 
-export default Box
+export default Box;
